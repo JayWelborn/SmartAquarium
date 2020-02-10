05861958326e3f1fdd1f4faa69240f2f903e82cf
jaywelborn <jesse.welborn@gmail.com>
2020-02-09 23:15:15 -0500

    home page header

--- client_web/src/components/home/index.js
+++ client_web/src/components/home/index.js
@@ -11,7 +11,18 @@ export default function Home(props) {
         <img class='logo' src='/assets/images/logo/white-on-transparent.svg' alt='Thermometer Logo'/>
         <h1>Aquatherm</h1>
         <h3>Healthy Fish Make Happy Fish</h3>
-
+        <div className="buttons">
+          <LinkContainer to='/login'>
+            <Button className='call-to-action'>
+              Log In
+            </Button>
+          </LinkContainer>
+          <LinkContainer to='/register'>
+            <Button className='call-to-action'>
+              Register
+            </Button>
+          </LinkContainer>
+        </div>
       </Jumbotron>
     </div>
   );
