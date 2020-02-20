9679127dba67dfed573fdb0c416461714a417711
jaywelborn <jesse.welborn@gmail.com>
2020-02-19 21:34:21 -0500

    added testimonial viewset

--- cloud/thermometer/utils/permissions.py
+++ cloud/thermometer/utils/permissions.py
@@ -37,6 +37,11 @@ class IsUserOrReadOnly(permissions.BasePermission):
     staff to eit all profiles
     """
 
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
